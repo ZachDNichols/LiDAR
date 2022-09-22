5480c67671ab19c796f3e24a54e3879e557e8b04
Zach Nichols <zachdnichols@gmail.com>
2022-09-21 21:40:31 -0500

    Create Project

---
+++ LiDAR/LiDAR/Intermediate/Build/Win64/LiDAREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.33.31630
+#include "C:/Users/zach/Documents/GitHub/LiDAR/LiDAR/LiDAR/Intermediate/Build/Win64/LiDAREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ LiDAR/LiDAR/Intermediate/Build/Win64/UnrealEditor/Inc/LiDAR/LiDAR.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeLiDAR_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_LiDAR;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_LiDAR()
+	{
+		if (!Z_Registration_Info_UPackage__Script_LiDAR.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/LiDAR",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x5F2E8A2E,
+				0x9F811252,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_LiDAR.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_LiDAR.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_LiDAR(Z_Construct_UPackage__Script_LiDAR, TEXT("/Script/LiDAR"), Z_Registration_Info_UPackage__Script_LiDAR, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x5F2E8A2E, 0x9F811252));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
