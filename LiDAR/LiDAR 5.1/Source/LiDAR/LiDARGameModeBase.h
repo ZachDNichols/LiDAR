bd11555decb04118e51a7165155c36277cc2101b
Zach Nichols <zachdnichols@gmail.com>
2023-02-09 15:46:25 -0600

    Created 5.1 blank project

---
+++ LiDAR/LiDAR 5.1/Source/LiDAR/LiDARGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LiDARGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LIDAR_API ALiDARGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
