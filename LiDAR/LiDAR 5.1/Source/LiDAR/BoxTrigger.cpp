cef3f2a6f7430ea4be15b07f8418b253f0f9d3f6
Zach Nichols <zachdnichols@gmail.com>
2023-03-24 15:05:47 -0500

    Redid Trigger System

---
+++ LiDAR/LiDAR 5.1/Source/LiDAR/BoxTrigger.cpp
@@ -0,0 +1,208 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BoxTrigger.h"
+#include "Interaction/InteractableInterface.h"
+#include "Kismet/GameplayStatics.h"
+
+ABoxTrigger::ABoxTrigger()
+{
+	OnActorBeginOverlap.AddDynamic(this, &ABoxTrigger::OnBeginOverlap);
+}
+
+void ABoxTrigger::OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor)
+{
+	if (bMultipleIDs && TargetObjects.Num() > 0)
+	{
+		TriggerInteraction(0);
+	}
+	else
+	{
+		TriggerInteraction();
+	}
+}
+
+void ABoxTrigger::TriggerInteraction()
+{
+	USoundBase* Sound = TargetObject.Sound;
+	
+	if (TargetObject.bJustSound)
+	{
+		if (SoundAttenuation)
+		{
+			if (Sound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.0f, 1.0f, 0.0f, SoundAttenuation);
+			}
+		}
+		else
+		{
+			if (Sound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.f, 1.f, 0.f);
+			}
+		}
+	}
+	else
+	{
+		if (TargetObject.bHaveActionWait)
+		{
+			const float SoundDuration = Sound->GetDuration();
+
+			if (SoundAttenuation)
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.0f, 1.0f, 0.0f, SoundAttenuation);
+				}
+			}
+			else
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.f, 1.f, 0.f);
+				}
+			}
+
+			FTimerHandle InteractionTimer;
+			FTimerDelegate InteractionDel;
+			InteractionDel.BindUFunction(this, FName("Interaction"), TargetObject.ObjectID, TargetObject.bInteractCall);
+			GetWorldTimerManager().SetTimer(InteractionTimer, InteractionDel, SoundDuration, false);
+		}
+		else
+		{
+			if (SoundAttenuation)
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.0f, 1.0f, 0.0f, SoundAttenuation);
+				}
+			}
+			else
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.f, 1.f, 0.f);
+				}
+			}
+			Interaction(TargetObject.ObjectID, TargetObject.bInteractCall);
+		}
+	}
+}
+
+void ABoxTrigger::TriggerInteraction(int InteractionIndex)
+{
+	if (TargetObjects.Num() <= InteractionIndex)
+	{
+		return;
+	}
+	
+	int ObjectID = TargetObjects[InteractionIndex].ObjectID;
+	bool bInteractCall = TargetObjects[InteractionIndex].bInteractCall;
+	USoundBase* Sound = TargetObjects[InteractionIndex].Sound;
+
+	if (TargetObjects[InteractionIndex].bJustSound)
+	{
+		const float SoundDuration = Sound->GetDuration();
+		
+		if (SoundAttenuation)
+		{
+			if (Sound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.0f, 1.0f, 0.0f, SoundAttenuation);
+			}
+		}
+		else
+		{
+			if (Sound)
+			{
+				UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.f, 1.f, 0.f);
+			}
+		}
+
+		FTimerHandle NextFunctionCallTimer;
+		FTimerDelegate NextFunctionCallDel;
+		NextFunctionCallDel.BindUFunction(this, FName("TriggerInteraction"), InteractionIndex + 1);
+
+		GetWorldTimerManager().SetTimer(NextFunctionCallTimer, NextFunctionCallDel, SoundDuration, false);	
+	}
+	else
+	{
+		if (TargetObjects[InteractionIndex].bHaveActionWait)
+		{
+			const float SoundDuration = Sound->GetDuration();
+
+			if (SoundAttenuation)
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.0f, 1.0f, 0.0f, SoundAttenuation);
+				}
+			}
+			else
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.f, 1.f, 0.f);
+				}
+			}
+
+			FTimerHandle InteractionTimer, NextFunctionCallTimer;
+		
+			FTimerDelegate ActionTimerDel;
+			ActionTimerDel.BindUFunction(this, FName("Interaction"), ObjectID, bInteractCall);
+		
+			GetWorldTimerManager().SetTimer(InteractionTimer, ActionTimerDel, SoundDuration, false);
+
+			FTimerDelegate NextFunctionCallDel;
+			NextFunctionCallDel.BindUFunction(this, FName("TriggerInteraction"), InteractionIndex + 1);
+
+			GetWorldTimerManager().SetTimer(NextFunctionCallTimer, NextFunctionCallDel, SoundDuration + 0.1f, false);
+		}
+		else
+		{
+			const float SoundDuration = Sound->GetDuration();
+
+			if (SoundAttenuation)
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.0f, 1.0f, 0.0f, SoundAttenuation);
+				}
+			}
+			else
+			{
+				if (Sound)
+				{
+					UGameplayStatics::PlaySoundAtLocation(GetWorld(), Sound, GetActorLocation(), 1.f, 1.f, 0.f);
+				}
+			}
+
+			Interaction(ObjectID, bInteractCall);
+
+			FTimerHandle NextFunctionCallTimer;
+			FTimerDelegate NextFunctionCallDel;
+
+			NextFunctionCallDel.BindUFunction(this, FName("TriggerInteraction"), InteractionIndex + 1);
+			GetWorldTimerManager().SetTimer(NextFunctionCallTimer, NextFunctionCallDel, SoundDuration, false);
+		}
+	}
+}
+
+void ABoxTrigger::Interaction(int InteractionID, bool bInteractCall)
+{
+	//Gets array of every actor that has the InteractableInterface
+	TArray<AActor*> InteractableActors;
+	UGameplayStatics::GetAllActorsWithInterface(GetWorld(), UInteractableInterface::StaticClass(), InteractableActors);
+
+	//Iterates through each actor and determines if it shares the same IDs that are supposed to be interacted with
+	for (AActor* Actor : InteractableActors)
+	{
+		int ObjectID = IInteractableInterface::Execute_GetObjectID(Actor);
+			if (InteractionID == ObjectID)
+			{
+				IInteractableInterface::Execute_Interact(Actor, bInteractCall);
+			}
+	}
+}
+
---
+++ LiDAR/LiDAR 5.1/Source/LiDAR/BoxTrigger.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "BoxTrigger.generated.h"
+
+USTRUCT(BlueprintType)
+struct FTargetObject
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Object")
+		bool bJustSound = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Object", meta = (EditCondition = "!bJustSound"))
+		int ObjectID;
+
+	//Used for determining what the interaction will be called with
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Object", meta = (EditCondition = "!bJustSound"))
+		bool bInteractCall = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Triggered Audio")
+	USoundBase* Sound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Triggered Audio", meta = (EditCondition = "!bJustSound"))
+	bool bHaveActionWait = false;
+};
+
+UCLASS()
+class LIDAR_API ABoxTrigger : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABoxTrigger();
+
+	//Properties for the trigger
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Trigger Properties")
+		bool bMultipleIDs = false;
+	
+	//Target IDs that can be interacted with
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Trigger Properties", meta = (EditCondition = "bMultipleIDs"))
+		TArray<FTargetObject> TargetObjects;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Trigger Properties", meta = (EditCondition = "!bMultipleIDs"))
+		FTargetObject TargetObject;
+
+	//Boolean that controls if the trigger is disabled
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Trigger")
+		bool bIsDisabled;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Trigger Properties")
+		USoundAttenuation* SoundAttenuation;
+
+	//Function for the overlap
+	UFUNCTION()
+		void OnBeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+private:
+	//Boolean to be used if the object is triggered
+	bool isTriggered;
+	void TriggerInteraction();
+	void TriggerInteraction(int InteractionIndex);
+	void Interaction(int InteractionID, bool bInteractCall);
+};
+
