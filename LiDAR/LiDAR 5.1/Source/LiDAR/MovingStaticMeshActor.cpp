2874f7b70106ec6d122f8239b1c2f8878d22598d
Zach Nichols <zachdnichols@gmail.com>
2023-03-24 00:11:37 -0500

    Fixed flickering issue with lemon

--- LiDAR/LiDAR 5.1/Source/LiDAR/MovingStaticMeshActor.cpp
+++ LiDAR/LiDAR 5.1/Source/LiDAR/MovingStaticMeshActor.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "Components/MovableStaticMeshComponent.h"
 #include "MovingStaticMeshActor.h"
+#include "Components/MovableStaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
 
 // Sets default values
