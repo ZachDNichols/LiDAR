515ebf9b166018e6d74e775a636b40647f26826c
Zach Nichols <zachdnichols@gmail.com>
2023-05-25 21:50:40 -0500

    Added base class for things like triggers, this way other classes don't have to use multiple FStructs

---
+++ LiDAR/Source/LiDAR/LidarTriggerBaseClass.cpp
@@ -0,0 +1,12 @@
+// Copyright 2023 Zach Nichols, All Rights Reserved
+
+
+#include "LidarTriggerBaseClass.h"
+
+LidarTriggerBaseClass::LidarTriggerBaseClass()
+{
+}
+
+LidarTriggerBaseClass::~LidarTriggerBaseClass()
+{
+}
